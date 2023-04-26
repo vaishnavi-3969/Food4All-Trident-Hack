6401a1ade6a7deaa9021103a7677367d264783c4
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-04-27 03:33:53 +0530

    pastwork done

--- src/components/Pastwork.js
+++ src/components/Pastwork.js
@@ -1,24 +1,159 @@
-import React from 'react'
-import '../assets/img/Carousel/1.png';
-import '../assets/img/Carousel/2.png';
-import '../assets/img/Carousel/3.png';
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import meter1 from '../assets/img/Carousel/1.png';
+import meter2 from '../assets/img/Carousel/2.png';
+import meter3 from '../assets/img/Carousel/3.png';
+import meter4 from '../assets/img/Carousel/4.png';
+import meter5 from '../assets/img/Carousel/5.png'
+import meter6 from '../assets/img/Carousel/6.png'
+import meter7 from '../assets/img/Carousel/7.png'
+import meter8 from '../assets/img/Carousel/8.png'
+import meter9 from '../assets/img/Carousel/9.png'
+import meter10 from '../assets/img/Carousel/10.png'
+import meter11 from '../assets/img/Carousel/11.png'
+import meter12 from '../assets/img/Carousel/12.png'
+import meter13 from '../assets/img/Carousel/13.png'
+import meter14 from '../assets/img/Carousel/14.png'
+import meter15 from '../assets/img/Carousel/15.png'
+import meter16 from '../assets/img/Carousel/16.png'
 
+import '../App.css';
+import './Pastwork.css'
 
 export const Pastwork = () => {
   return (
-    <div>
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img className="d-block w-100" src="https://images.unsplash.com/photo-1517436073-3b3b1b1b1b1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZCUyMGNvbG9yJTIwZm9yJTIwZmFjZSUyMGNvbXBhbnl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="First slide"/>
-                    </div>
-                    </div>
-                    </div>
-    </div>
+    <section className='pastwork' id='pastwork'>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">Past Work</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <Carousel className="custom-carousel">
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter1}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Annapurna</h3>
+                  <p>The Annapurna food drive campaign collects and distributes food items to people in need, including those affected by poverty, natural disasters, and other crises. The campaign collects a variety of food items, including grains, pulses, oil, sugar, and spices.</p>
+                  <button className="btn btn-primary">Learn more</button>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter2}
+                  alt="Second slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter3}
+                  alt="Third slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter4}
+                  alt="Fourth slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter5}
+                  alt="fifth slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={meter6}
+                  alt="sixth slide"
+                />
+              </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter7}
+                    alt="seventh slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter8}
+                    alt="eighth slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter9}
+                    alt="ninth slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter10}
+                    alt="tenth slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter11}
+                    alt="eleventh slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter12}
+                    alt="twelfth slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter13}
+                    alt="eleventh slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter14}
+                    alt="eleventh slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter15}
+                    alt="eleventh slide"
+                />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={meter16}
+                    alt="eleventh slide"
+                />
+                </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
+
