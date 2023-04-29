f2648f8f43d40d3fc8107d914737ceb48a038e02
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-04-29 15:57:03 +0530

    updated donatefunds

--- src/components/DonateFunds.js
+++ src/components/DonateFunds.js
@@ -1,7 +1,7 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Button, Table } from 'react-bootstrap';
 import './Donation.css';
-import { getDatabase, ref, push ,onValue} from "firebase/database";
+import { getDatabase, ref, push, onValue } from "firebase/database";
 import { initializeApp } from "firebase/app";
 const DonateFunds = () => {
   const [donationAmount, setDonationAmount] = useState('');
@@ -11,7 +11,7 @@ const DonateFunds = () => {
   const [location, setLocation] = useState('');
   const [donationSuccess, setDonationSuccess] = useState(false);
   const [donationError, setDonationError] = useState(null);
-  
+  const [totalDonations, setTotalDonations] = useState(0);
   const firebaseConfig = {
     apiKey: "AIzaSyBcAR9bOUavF6LpUD1NixXMoAOV_R7sqzo",
     authDomain: "food4all-6aadf.firebaseapp.com",
@@ -25,7 +25,7 @@ const DonateFunds = () => {
   const database = getDatabase(app);
   const donationsRef = ref(database, 'donations');
   const [donations, setDonations] = useState([]);
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -57,6 +57,7 @@ const DonateFunds = () => {
           return { id: key, ...data[key] };
         });
         setDonations(donationsArray);
+        setTotalDonations(donationsArray.reduce((acc, curr) => acc + parseInt(curr.donationAmount), 0));
       } else {
         setDonations([]);
       }
@@ -69,6 +70,11 @@ const DonateFunds = () => {
       <p>Your donation will help support our cause to provide food to those in need. Thank you for your generosity!</p>
       {donationSuccess && <p className="success-message">Thank you for your donation!</p>}
       {donationError && <p className="error-message">{donationError}</p>}
+      
+      <div className="total-donations">
+      <p className="total-donations-text">Total Donations Raised: Rs.{donations.reduce((acc, curr) => acc + Number(curr.donationAmount), 0)}</p>
+    </div>
+
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="donationAmount">
           <Form.Label>Donation Amount</Form.Label>
@@ -94,36 +100,37 @@ const DonateFunds = () => {
           Donate
         </Button>
       </Form>
+      <h1>Donation List</h1>
       <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>Donation Amount</th>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Phone Number</th>
-      <th>Location</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    
-      <td>{donationAmount}</td>
-      <td>{name}</td>
-      <td>{email}</td>
-      <td>{phone}</td>
-      <td>{location}</td>
-    </tr>
-    {donations.map((donation) => (
-          <tr key={donation.id}>
-            <td>{donation.donationAmount}</td>
-            <td>{donation.name}</td>
-            <td>{donation.email}</td>
-            <td>{donation.phone}</td>
-            <td>{donation.location}</td>
+        <thead>
+          <tr>
+            <th>Donation Amount</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Location</th>
           </tr>
-        ))}
-  </tbody>
-</Table>
+        </thead>
+        <tbody>
+          <tr>
+
+            <td>{donationAmount}</td>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>{phone}</td>
+            <td>{location}</td>
+          </tr>
+          {donations.map((donation) => (
+            <tr key={donation.id}>
+              <td>{donation.donationAmount}</td>
+              <td>{donation.name}</td>
+              <td>{donation.email}</td>
+              <td>{donation.phone}</td>
+              <td>{donation.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
